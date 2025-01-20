224db2fb398a76026d4ad51152af8ea3b0df4431
Koushik Saha <128729555+koushiksaha003@users.noreply.github.com>
2025-01-20 19:04:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,43 @@
-const express =require ("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const colors = require("colors");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-const path = require("path")
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+import path from 'path';
+import userRoutes from './routes/userRoutes';
+import blogRoutes from './routes/blogRoutes';
+
 //env config
 dotenv.config();
-
-//router import
-const userRoutes = require("./routes/userRoutes");
-const blogRoutes = require("./routes/blogRoutes");
 
 //mongodb connection
 connectDB();
 
-const __dirname=path.resolve();
-//rest objecct
+const __dirname = path.resolve();
+//rest object
 const app = express();
 
-//middelwares
+//middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 //routes
-app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/blog", blogRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/blog', blogRoutes);
 
-app.get('*',(req,res)=>
-{
-  res.sendFile(path.join(__dirname,'client','build','index.html'))
-}
-)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
-
-app.use(express.static(path.join(__dirname,'/client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 // Port
 const PORT = process.env.PORT || 8080;
 //listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan
-      .white
+    `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan.white
   );
 });
