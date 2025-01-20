ef7f339ae50e097a6b8b8ec0c28f498766e5252c
Koushik Saha <128729555+koushiksaha003@users.noreply.github.com>
2025-01-20 19:08:03 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,18 @@
-const mongoose = require("mongoose");
-const colors = require("colors");
+import mongoose from 'mongoose';
+import colors from 'colors';
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(
-      `Connected to Mongodb Database ${mongoose.connection.host}`.bgMagenta
-        .white
+      `Connected to Mongodb Database ${mongoose.connection.host}`.bgMagenta.white
     );
   } catch (error) {
     console.log(`MONGO Connect Error ${error}`.bgRed.white);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
