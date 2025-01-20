e79aec956e0f6fdaf65dcb6a601d6961e0e9a253
Koushik Saha <128729555+koushiksaha003@users.noreply.github.com>
2025-01-20 19:29:58 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
@@ -26,4 +26,4 @@ const userSchema = new mongoose.Schema(
 
 const userModel = mongoose.model("User", userSchema);
 
-module.exports = userModel;
+export default userModel;
